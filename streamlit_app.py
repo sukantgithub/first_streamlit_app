7987db327a0da876cfcb9fbe8e0976962ab00b1f
sukantgithub <130145270+sukantgithub@users.noreply.github.com>
2023-05-15 13:08:12 +0100

    Update streamlit_app.py
    Header changed and Emojies added.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title("My Parents new heathy diner.")
 
-streamlit.title("🥣 Breakfast Menu!")
-streamlit.text("🥗 Omega 3 & Blueberry Oatmeal")
-streamlit.text("🐔 Kale, Spinach & Rocket Smoothie")
-streamlit.text("🥑🍞 Hard-Boiled Free-range Egg")
+streamlit.title("Breakfast favorites")
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-range Egg")
+streamlit.text("🥑🍞 Avacado Toast ")
