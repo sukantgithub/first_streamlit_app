8725cc46cf4aa4cc166e8f910b8e0507bc9380ae
sukantgithub <130145270+sukantgithub@users.noreply.github.com>
2023-05-15 15:53:09 +0100

    Update streamlit_app.py
    Enhanced data frame to pick selected fruits only

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ streamlit.text("🥑🍞 Avacado Toast ")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits: " , list(my_fruit_list.index))
-streamlit.dataframe(my_fruit_list)
+fruits_selected = streamlit.multiselect("Pick some fruits: " , list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc(fruit_selected)
+streamlit.dataframe(fruits_to_show)
 
