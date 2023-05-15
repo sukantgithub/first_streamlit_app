1e9a3afd2b38acfc28d310f8b0b2eaca562e004b
sukantgithub <130145270+sukantgithub@users.noreply.github.com>
2023-05-15 17:01:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,8 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 fruits_selected = streamlit.multiselect("Pick some fruits: " , list(my_fruit_list.index), ['Avocado','Apple'])
-#if fruits_selected <> '' :
-  fruits_to_show = my_fruit_list.loc[fruits_selected]
-  streamlit.dataframe(fruits_to_show)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
 #else :
  # streamlit.dataframe(my_fruit_list)
 
