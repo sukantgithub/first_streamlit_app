1285abe20054f8ea7b7ab787678e3a4540e8ed91
sukantgithub <130145270+sukantgithub@users.noreply.github.com>
2023-05-15 15:36:21 +0100

    Update streamlit_app.py
    Multi select fruits with pandas ata frame display

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 streamlit.title("My Parents new heathy diner.")
 
 streamlit.header("Breakfast favorites")
@@ -8,3 +11,7 @@ streamlit.text("🐔 Hard-Boiled Free-range Egg")
 streamlit.text("🥑🍞 Avacado Toast ")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect(“Pick some fruits: ” , list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
+
