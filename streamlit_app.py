4488dcc6c079a5a363343ce8e1427e6d889d6ddf
sukantgithub <130145270+sukantgithub@users.noreply.github.com>
2023-05-15 16:22:53 +0100

    Update streamlit_app.py
    Extended feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,9 @@ streamlit.text("🥑🍞 Avacado Toast ")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits: " , list(my_fruit_list.index), ['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(fruits_to_show)
+if fruits_selected = '':
+  streamlit.dataframe(my_fruit_list)
+else:
+  fruits_to_show = my_fruit_list.loc[fruits_selected]
+  streamlit.dataframe(fruits_to_show)
 
