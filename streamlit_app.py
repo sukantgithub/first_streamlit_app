5819dbdd287454485533f43069b415d8354b74a6
sukantgithub <130145270+sukantgithub@users.noreply.github.com>
2023-05-15 16:32:45 +0100

    Update streamlit_app.py
    trial fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # fruits_selected = streamlit.multiselect("Pick some fruits: " , list(my_fruit_list.index), ['Avocado','Strawberries'])
 fruits_selected = streamlit.multiselect("Pick some fruits: " , list(my_fruit_list.index), ['X'])
-
-if fruits_selected == 'X':
-  streamlit.dataframe(my_fruit_list)
-else:
-  fruits_to_show = my_fruit_list.loc[fruits_selected]
-  streamlit.dataframe(fruits_to_show)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
 
