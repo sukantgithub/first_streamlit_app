5127761641e9d75d9a703718751ce8db929eee09
sukantgithub <130145270+sukantgithub@users.noreply.github.com>
2023-05-17 15:51:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? : panda bring json format data to normalised tabular format
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
