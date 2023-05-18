df86ed992ff6d9950c7ce9d013a490385a2b8643
sukantgithub <130145270+sukantgithub@users.noreply.github.com>
2023-05-18 23:55:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
+
 
 def insert_row_snowflake(newfruit):
   with my_cnx.cursor() as my_cur:
